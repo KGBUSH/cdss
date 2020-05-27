9b5f5f723efd5d05698d7823a353db35592d9348
邓扬 <dengyang@dengyangdeMacBook-Pro.local>
2020-05-27 14:58:36 +0800

    [upd] duration归一化到hour，待机

--- utils/duration.py
+++ utils/duration.py
@@ -16,6 +16,17 @@ duration_rules = {
     '##m 小##a 时##ng': '小时'
 }
 
+time_nomalize = {
+    '天': 24,
+    '周': 7 * 24,
+    '月': 30 * 7 * 24,
+    '年': 12 * 30 * 7 * 24
+}
+
+digit_dict = {"零": 0, "一": 1, "二": 2, "两": 2, "俩": 2, "三": 3,
+              "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9}
+
+
 prog = re.compile('([一二三四五六七八九十]|(([0-9]*)\.)?([0-9]*)(\+|余)?|半|多)?(个)?(月|天|小时|年|周|次)(半|余)?')
 
 
@@ -55,3 +66,28 @@ class Duration(object):
             return '半年'
         num = sentence.split(flag)[0].split(' ')[-1]  # 拿到数字
         return str(num) + duration_rules[flag]
+
+    @staticmethod
+    def normalize_time(duration):
+        """
+        统一为小时h
+        :param duration: get_duration_re 中提取出来的duration
+        :return:
+        """
+        for key in time_nomalize.keys():
+            if key in duration:
+                tmp_list = duration.split(key)
+                num = -1
+                try:
+                    num = int(tmp_list[0])
+                except ValueError:
+                    try:
+                        num = digit_dict[tmp_list[0]]
+                    except:
+                        raise ValueError
+                num *= time_nomalize[key]
+                num = str(num)  # 最后再转回str
+                if tmp_list[-1] == '+':
+                    num += '+'
+                return num
+
